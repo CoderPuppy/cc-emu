165fc0f0735eda779e92b492dfa059f2e8e05504
CoderPuppy <cpup@cpup-laptop>
2014-02-08 22:07:55 -0500

    Sortof add support for passing the dir for the emulator to run in

--- cli.lua
+++ cli.lua
@@ -2,9 +2,11 @@
 
 io.stdout:setvbuf('no')
 
-local dir = '/home/cpup/code/lua/cc-emu/dev'
-
 local path = require 'pl.path'
 local emu = require 'emu'
+
+-- local dir = ...
+local dir = '/home/cpup/code/lua/cc-emu/dev'
+dir = path.normpath(path.join(path.currentdir(), dir))
 
 emu(dir)
