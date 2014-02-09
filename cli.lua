36af436e8207b9be84a43c7507520f3eba98a972
CoderPuppy <cpup@cpup-laptop>
2014-02-08 22:24:24 -0500

    Enable passing the directory as an argument

--- cli.lua
+++ cli.lua
@@ -5,8 +5,8 @@ io.stdout:setvbuf('no')
 local path = require 'pl.path'
 local emu = require 'emu'
 
--- local dir = ...
-local dir = '/home/cpup/code/lua/cc-emu/dev'
+local dir = ...
+-- local dir = '/home/cpup/code/lua/cc-emu/dev'
 dir = path.normpath(path.join(path.currentdir(), dir))
 
 emu(dir)
