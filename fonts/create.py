859722fea0ed205c1af37c43c211d2f2855d22fc
CoderPuppy <coderpup@gmail.com>
2016-07-28 12:06:55 -0400

    Add "termu" to the name of the font

--- fonts/create.py
+++ fonts/create.py
@@ -1,3 +1,7 @@
 import fontforge
 
-fontforge.open('input.otf').save('termu.sfd')
+font = fontforge.open('input.otf')
+font.fontname = 'Termu-' + font.fontname
+font.familyname = 'Termu: ' + font.familyname
+font.fullname = 'Termu: ' + font.fullname
+font.save('termu.sfd')
