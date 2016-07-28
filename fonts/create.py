972512beb44feacd7448a30451d2b92996d0a0c8
CoderPuppy <coderpup@gmail.com>
2016-07-28 14:55:29 -0400

    Fix the familyname

--- fonts/create.py
+++ fonts/create.py
@@ -2,6 +2,6 @@ import fontforge
 
 font = fontforge.open('input.otf')
 font.fontname = 'Termu-' + font.fontname
-font.familyname = 'Termu: ' + font.familyname
+font.familyname = 'Termu ' + font.familyname
 font.fullname = 'Termu: ' + font.fullname
 font.save('termu.sfd')
