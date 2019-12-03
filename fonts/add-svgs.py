6f9d401054a7ee637e23ad438534a7e97ac8310f
CoderPuppy <coderpup@gmail.com>
2019-12-03 11:37:38 -0500

    Fix the font generation for the new font map

--- fonts/add-svgs.py
+++ fonts/add-svgs.py
@@ -1,5 +1,6 @@
 import re
 import fontforge
+import psMat
 
 font = fontforge.open('termu.sfd')
 
@@ -9,6 +10,7 @@ for i in range(256):
     width = glyph.width
     glyph.clear()
     glyph.importOutlines('glyph_' + str(i) + '.svg')
+    glyph.transform(psMat.scale(6/8, 9/11))
     glyph.transform((3.5087777777778, 0.0, 0.0, 3.5087719298246, 0, -640 -978.421 -22.160684210526 -7.5187857142857))
     glyph.width = 842.10666666667
 
